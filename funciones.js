150719c4bdf828865283cc79c061bd9aa1590492
Laura <laurasolispavon@gmail.com>
2024-05-25 18:46:20 +0200

    respaldo cambios

---
+++ funciones.js
@@ -0,0 +1,13 @@
+function calcularProducto(a, b) {
+    return a * b;
+}
+
+const resultado = calcularProducto(4, 3);
+console.log(resultado); // Imprime 12
+function fahrenheitACelsius(fahrenheit) {
+    return (5 / 9) * (fahrenheit - 32);
+}
+
+
+const temperaturaCelsius = fahrenheitACelsius(77);
+console.log(temperaturaCelsius); 
